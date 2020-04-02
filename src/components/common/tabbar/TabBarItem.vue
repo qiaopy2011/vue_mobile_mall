0a896a7c4dc4c3431b0b3972a7b4c965c265efa8
qiaopengyu <qiaopengyu@MacBookPro.local>
2020-04-02 09:06:41 +0800

    update

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: 'deeppink'
+      }
+    },
+    computed: {
+      /*
+      * 判断路由中的path参数是否包含自定义的path的值的内容。如果不为-1则能判断出当前活跃点击
+      * */
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    data() {
+      return {
+        // isActive: false
+      }
+    },
+    methods: {
+      itemClick() {
+        //  console.log("itemClick");
+        this.$router.replace(this.path).catch(err => {
+        })
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    height: 24px;
+    width: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+const Home = ()=>import('../views/home/Home')
+const Category = ()=>import('../views/category/Category')
+const ShopCart = ()=>import('../views/shopcart/ShopCart')
+const Profile = ()=>import('../views/profile/Profile')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:'',
+    redirect:'/home'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    component:Home
+  },
+  {
+    path: '/category',
+    component:Category
+  },
+  {
+    path: '/cart',
+    component:ShopCart
+  },
+  {
+    path: '/profile',
+    component:Profile
   }
 ]
 
